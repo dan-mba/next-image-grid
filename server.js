6ff26203965384ef817196df65dad27f61debf3a
Dan <35927536+dan-mba@users.noreply.github.com>
2020-12-10 10:34:00 -0500

    add rate limiting

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ const handle = app.getRequestHandler()
 
 app.prepare().then(async () => {
   const server = express();
+  
+  // set up rate limiter: maximum of 50 requests per minute
+  const RateLimit = require('express-rate-limit');
+  const limiter = new RateLimit({
+    windowMs: 1*60*1000, // 1 minute
+    max: 50
+  });
+
+  // apply rate limiter to all requests
+  server.use(limiter);
+
   await images.dbInit();
 
   server.get('/api/images/:id', (req, res) => {
