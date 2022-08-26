3d96b5a01144bc38ba39b6675fa3fa34ec3b7330
Dan <35927536+dan-mba@users.noreply.github.com>
2022-08-26 08:27:35 -0400

    break out dbInit into own file

--- util/database.js
+++ util/database.js
@@ -1,75 +1,26 @@
-const {Sequelize, DataTypes } = require('sequelize');
 const sanitizeHtml = require('sanitize-html');
+const {dbInit} = require('./pg');
 
 let sequelize = null;
 
-async function dbInit() {
-  sequelize = new Sequelize(
-    process.env.DATABASE_URL,
-    {
-      dialect: 'postgres',
-      protocol: 'postgres',
-      dialectOptions: {
-        ssl: {
-          rejectUnauthorized: false
-        }
-      }
-    }
-  );
+export async function getImages() {
+  if(!sequelize) sequelize = await dbInit();
 
-  return sequelize
-    .authenticate()
-    .then(() => {
-      console.log('Connection has been established successfully.');
-    
-      sequelize.define('image',{
-        id: {
-          type: DataTypes.INTEGER,
-          primaryKey: true
-        },
-        title: {
-          type: DataTypes.TEXT
-        },
-        caption: {
-          type: DataTypes.TEXT
-        },
-        img: {
-          type: DataTypes.TEXT
-        },
-        story: {
-          type: DataTypes.TEXT
-        },
-        original : {
-          type: DataTypes.TEXT
-        }
-      }, {
-        timestamps: false
-      });
-
-      return sequelize;
-    })
-    .catch(err => {
-      console.error('Unable to connect to the database:', err);
-    });
-}
-
-exports.getImages = async () => {
-  sequelize = await dbInit();
-  return sequelize.models.image.findAll({attributes: ['id', 'title', 'img']})
-    .then(images => {
-      return(images);
-    }).catch(() => {
-      return([]);
-    })
+  try {
+    return await sequelize.models.image.findAll({attributes: ['id', 'title', 'img']})
+  } catch {
+    return [];
   }
+}
   
-exports.getImageById = async (id) => {
-  sequelize = await dbInit();
-  return sequelize.models.image.findByPk(id,{raw: true})
-    .then(image => {
-      const clean = {...image, story: sanitizeHtml(image.story)}
-      return(clean);
-    }).catch(() => {
-      return(null);
-    })
+export async function getImageById(id) {
+  if(!sequelize) sequelize = await dbInit();
+
+  try {
+    const image = await sequelize.models.image.findByPk(id,{raw: true})
+    return {...image, story: sanitizeHtml(image.story)}
+  }
+  catch {
+    return null;
+  }
 }
