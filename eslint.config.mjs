aa5ee9918fc86300f712b84d3f87c98e5b1c48cd
Dan <35927536+dan-mba@users.noreply.github.com>
2025-10-31 06:34:37 -0400

    update lint config for next 16

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,21 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals"),
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+ 
+const eslintConfig = defineConfig([
+  ...nextVitals,
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
     rules: {
-      "@next/next/no-img-element": "off"
+      '@next/next/no-img-element': 'off'
     }
   },
-];
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+]);
 
 export default eslintConfig;
